3fdfb29ece946ac1b3ea501f4a0aff8b761364df
lhp <2771287337@qq.com>
2018-12-06 22:19:24 +0800

    项目初始化

---
+++ src/cn/jiyun/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.jiyun;
+
+/**
+ * @author lhp
+ * @create 2018-10-22 23:35
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
