32ec9a2574b98817c03b0d546313edf51c3fdde0
lhp <2771287337@qq.com>
2018-12-06 22:40:18 +0800

    创建文件，并输出一句话！

---
+++ src/cn/jiyun/TestGit.java
@@ -0,0 +1,12 @@
+package cn.jiyun;
+
+/**
+ * @author lhp
+ * @create 2018-12-06 22:35
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
